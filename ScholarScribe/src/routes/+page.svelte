1cccda945940257a7081d4de10ed2876004a6765
Matt Dembny <matthewdembny@gmail.com>
2024-02-01 12:35:47 -0600

    Added adapter for node http build

--- ScholarScribe/src/routes/+page.svelte
+++ ScholarScribe/src/routes/+page.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <div class="container mx-auto p-8 space-y-8">
-	<h1 class="h1">Hello Skeleton</h1>
-	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+	<h1 class="h1">Welcome to Scholar Scribe</h1>
+	<p>A Web Based Citation Engine</p>
 	<section>
 		<a class="btn variant-filled-primary" href="https://kit.svelte.dev/">SvelteKit</a>
 		<a class="btn variant-filled-secondary" href="https://tailwindcss.com/">Tailwind</a>
