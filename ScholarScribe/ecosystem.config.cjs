df6ad884940ea6aa09d79470a001ae04109626b5
Matt Dembny <87963417+MattGet@users.noreply.github.com>
2024-02-02 18:30:56 -0600

    Built for production on ramen.valpo.edu
    configured pm2 setup

---
+++ ScholarScribe/ecosystem.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+	/**
+	 * Application configuration section
+	 * http://pm2.keymetrics.io/docs/usage/application-declaration/
+	 */
+	apps: [
+		// First application
+		{
+			name: 'ScholarScribe', // name of the process in PM2
+			script: 'build/index.js',
+			env_production: {
+				NODE_ENV: 'production',
+				PORT: 5173 // port the app will be launched on
+			},
+            env:{
+                PORT: 5173, // port the app will be launched on
+                ORIGIN: 'http://ramen.valpo.edu'
+            },
+            watch: true,
+            ignore_watch: ['node_modules'],
+		}
+	]
+};
