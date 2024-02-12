2bb887e90647df1d6615f550359e40011e4a630f
Matt Dembny <matthewdembny@gmail.com>
2024-02-12 14:37:04 -0600

    fixed issue with initial export func

--- ScholarScribe/src/lib/client/export.funcs.ts
+++ ScholarScribe/src/lib/client/export.funcs.ts
@@ -1,6 +1,5 @@
 export function exportJSON(source: any) {
-    var text = JSON.stringify(source);
-    var jsonse = JSON.stringify(text);
+    var jsonse = JSON.stringify(source);
     var blob = new Blob([jsonse], { type: 'application/json' });
     var url = URL.createObjectURL(blob);
     var a = document.createElement('a');
