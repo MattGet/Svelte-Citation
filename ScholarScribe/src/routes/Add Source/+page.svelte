2aa2fe3bdeb87bfc5aeb29638a616b9b961c06fa
shdynasty <cgbehnke@gmail.com>
2024-03-18 11:50:25 -0500

    Bibtex compliant and new sources added
    need to update the update page

--- ScholarScribe/src/routes/Add Source/+page.svelte
+++ ScholarScribe/src/routes/Add Source/+page.svelte
@@ -2,6 +2,8 @@
 	import BookForm from '$lib/components/BookForm.svelte';
 	import JournalForm from '$lib/components/JournalForm.svelte';
 	import WebForm from '$lib/components/WebForm.svelte';
+	import PatentForm from '$lib/components/PatentForm.svelte';
+	import MagazineForm from '$lib/components/MagazineForm.svelte';
 	import '../../app.postcss';
 
 	// Floating UI for Popups
@@ -19,17 +21,23 @@
 	<div class="space px-10 pt-10">
 		<select class="select" size="1" bind:value={form}>
 			<option value="webpage">Website</option>
-			<option value="article">Journal</option>
+			<option value="journal">Journal</option>
 			<option value="book">Book</option>
+			<option value="patent">Patent</option>
+			<option value="magazine">Editorial</option>
 		</select>
 	</div>
 
 	{#if form == 'webpage'}
 		<WebForm user={user?.id} />
-	{:else if form == 'article'}
+	{:else if form == 'journal'}
 		<JournalForm user={user?.id} />
 	{:else if form == 'book'}
 		<BookForm user={user?.id} />
+	{:else if form == 'patent'}
+		<PatentForm user={user?.id} />
+	{:else if form == 'magazine'}
+		<MagazineForm user={user?.id} />
 	{:else}
 		<h1 class="h1">
 			<span
